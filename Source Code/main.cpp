6182a776236de6ed626b2db18344dbc1f61fb129
Amr El-Dahshan <144555367+Amr-Khaled-Ahmed@users.noreply.github.com>
2024-10-24 00:40:24 +0300

    Update main.cpp

--- Source Code/main.cpp
+++ Source Code/main.cpp
@@ -8,9 +8,9 @@ int main() {
     {
         string choice;
         do {
-            cout << "\033[0m" << "Choose\n"
-                 << "1 - To load a new program\n"
-                 << "2 - To exit\n";
+            cout << "Choose\n"
+                << "1 - To load a new program\n"
+                << "2 - To exit\n";
             cin >> choice;
             if (choice == "2") {
                 return 0;
@@ -20,10 +20,16 @@ int main() {
                 cout << "Choose if you want to continue\n";
             }
             else {
-                cout << "\033[1;31m"  << "Some thing happen wrong\nPlease choose a right option" << endl;
+                cerr << "Some thing happen wrong\nPlease choose a right option" << endl;
             }
 
         } while (choice != "2" || choice != "1");
     }
+
+
+
+
+
+
 
 }
